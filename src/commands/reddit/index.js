28ddfed30834cccc040eb2285a8e855dff0db427
alefherrera <alef.herrera@gmail.com>
2016-02-11 00:11:37 -0300

    reddit

--- src/commands/reddit/index.js
+++ src/commands/reddit/index.js
@@ -5,6 +5,6 @@ const request = Promise.promisify(_request);
 export default {
 	reddit: (bot, msg, suffix) => {
     	const command = suffix.toLowerCase().split(' ')[0];
-    	return request('www.reddit.com/r/random.json').then(data => bot.sendMessage(msg.channel, data));
+    	return request('www.reddit.com/r/random/random.json').then(data => bot.sendMessage(msg.channel, data));
     }
 }
