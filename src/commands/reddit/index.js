6559a6a920b9207dbc11f43398f11fb45a8967aa
alefherrera <alef.herrera@gmail.com>
2016-02-11 00:35:32 -0300

    reddit

--- src/commands/reddit/index.js
+++ src/commands/reddit/index.js
@@ -5,6 +5,6 @@ const request = Promise.promisify(_request);
 export default {
 	reddit: (bot, msg, suffix) => {
     	const command = suffix.toLowerCase().split(' ')[0];
-    	return request('https://www.reddit.com/domain/imgur.com/random.json').then(data => bot.sendMessage(msg.channel, data[0].data.children[0].url));
+    	return request('https://www.reddit.com/domain/imgur.com/random.json').then(data => bot.sendMessage(msg.channel, JSON.stringify(data[0].data)));
     }
 }
