137a2625e7813d697fb9e42a736f415715154d51
alefherrera <alef.herrera@gmail.com>
2016-02-11 01:08:24 -0300

    reddit

--- src/commands/reddit/index.js
+++ src/commands/reddit/index.js
@@ -1,8 +1,10 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetc';
 
 export default {
 	reddit: (bot, msg, suffix) => {
     	const command = suffix.toLowerCase().split(' ')[0];
-    	return fetch('https://www.reddit.com/domain/imgur.com/random.json').then(resp => resp.json()).then(json => bot.sendMessage(msg.channel, JSON.stringify(json)));
+    	return fetch('https://www.reddit.com/domain/imgur.com/random.json')
+    	.then(resp => resp.json())
+    	.then(json => bot.sendMessage(msg.channel, json));
     }
 }
