0eeed3ca84fa254b950d28c66341fe3db9df4137
alefherrera <alef.herrera@gmail.com>
2016-02-11 01:11:10 -0300

    reddit

--- src/commands/reddit/index.js
+++ src/commands/reddit/index.js
@@ -5,6 +5,6 @@ export default {
     	const command = suffix.toLowerCase().split(' ')[0];
     	return fetch('https://www.reddit.com/domain/imgur.com/random.json')
     	.then(resp => resp.json())
-    	.then(json => bot.sendMessage(msg.channel, json));
+    	.then(json => bot.sendMessage(msg.channel, json[0].data.children[0].data.url));
     }
 }
