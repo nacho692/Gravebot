db1320b197bf7d30580e805e8feb4adb7ca58546
alefherrera <alef.herrera@gmail.com>
2016-02-11 00:08:49 -0300

    reddit

--- src/commands/reddit/index.js
+++ src/commands/reddit/index.js
@@ -1,6 +1,10 @@
+import Promise from 'bluebird';
+import _request from 'request';
+const request = Promise.promisify(_request);
+
 export default {
 	reddit: (bot, msg, suffix) => {
     	const command = suffix.toLowerCase().split(' ')[0];
-    	bot.sendMessage('nacho puto');
+    	return request('www.reddit.com/r/random.json').then(data => bot.sendMessage(msg.channel, data));
     }
 }
